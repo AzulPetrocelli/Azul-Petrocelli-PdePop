a01a1c10bf18bc1799aae779ff188c5244d47f92
AzulPetrocelli <azulpetrocelli81@gmail.com>
2024-04-13 19:09:57 -0300

    prove y arregle la funcion ordenarCancionAlfabeticamente para que me retornara la cancion que tendria q ir primero

--- src/Library.hs
+++ src/Library.hs
@@ -1,10 +1,10 @@
 module Library where
 import PdePreludat
 
-data Cancion = unaCancion {
+data Cancion = UnaCancion {
     titulo :: String,
     duracionMinutos :: Number,
-    instrumentos :: [Instrumentos] 
+    instrumentos :: Instrumentos
 } deriving (Show)
 
 data Instrumentos = Acapella | Guitarra | Bajo | Bateria | Teclado  deriving (Show)
@@ -12,40 +12,24 @@ data Instrumentos = Acapella | Guitarra | Bajo | Bateria | Teclado  deriving (Sh
 
 -- canciones para la banda
 cancion1 :: Cancion
-cancion1 = unaCancion {
+cancion1 = UnaCancion {
     titulo = "Monoriel",
     duracionMinutos = 4,
-    instrumentos = [Teclado]
+    instrumentos = Teclado
 }
 
 cancion2 :: Cancion
-cancion2 = unaCancion {
+cancion2 = UnaCancion {
     titulo = "No vives de ensalada",
     duracionMinutos = 1,
-    instrumentos = [Acapella]
+    instrumentos = Acapella
 }
 
 -- ordenar por orden alfabetico
 retornarTitulo :: Cancion -> String
 retornarTitulo cancion = titulo cancion
 
-ordenarCancionAlfabeticamente :: Ord String => String -> String -> Cancion
+ordenarCancionAlfabeticamente :: Cancion -> Cancion -> String
 ordenarCancionAlfabeticamente cancionA cancionB
-    | retornarTitulo cancionA > retornarTitulo cancionB = cancionB
-    | retornarTitulo cancionA > retornarTitulo cancionB = cancionB
-
-
--- aceptacion por los fans y puntaje
-puntajeDeLaCancion :: Cancion -> Number
-puntajeDeLaCancion cancion 
-  |  head (titulo cancion) == "M" = 500
-  |  even (duracionMinutos cancion) = length (titulo cancion) * 10
-  |  instrumentos cancion == Acapella = 10
-
-aceptacionDelPublico :: Cancion -> Bool
-aceptacionDelPublico cancion = puntajeDeLaCancion cancion > 60
-
-
--- la cancion es acapela?
-esAcapela :: Cancion -> Bool
-esAcapela cancion = head (instrumentos cancion) == Acapella
+    | retornarTitulo cancionA < retornarTitulo cancionB = retornarTitulo cancionA
+    | retornarTitulo cancionA > retornarTitulo cancionB = retornarTitulo cancionB
